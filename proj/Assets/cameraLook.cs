6c2f7b440b32a832c86c2204e164c1bb1931a121
Nico Benedetti <r0969370@ucll.be>
2025-02-26 14:16:40 +0100

    rotation without weird bug

---
+++ proj/Assets/cameraLook.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class cameraLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 50f;
+    public Transform player;
+
+    private float xRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //this prevents looking too far up and down
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
