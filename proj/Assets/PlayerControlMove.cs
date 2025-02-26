a57aed37bbb5b470f5b126402495371765d146bc
Nico Benedetti <r0969370@ucll.be>
2025-02-26 13:24:07 +0100

    movement in all directions

--- proj/Assets/PlayerControlMove.cs
+++ proj/Assets/PlayerControlMove.cs
@@ -4,6 +4,7 @@ public class scriptComponent : MonoBehaviour
 {
 
     [SerializeField] public float forwardSpeed = 5f;
+    [SerializeField] public float rightSpeed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +16,18 @@ public class scriptComponent : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)){
         transform.transform.Translate(new Vector3(0, 0, forwardSpeed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.S)){
+        transform.transform.Translate(new Vector3(0, 0, -forwardSpeed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+        transform.transform.Translate(new Vector3(-rightSpeed * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.D)){
+        transform.transform.Translate(new Vector3(rightSpeed * Time.deltaTime, 0, 0));
         }
     }
 }
