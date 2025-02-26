3d8e9c702f7d75c15d2abf6d001523374896fc9e
Nico Benedetti <r0969370@ucll.be>
2025-02-26 13:10:18 +0100

    movement with public variable

--- proj/Assets/PlayerControlMove.cs
+++ proj/Assets/PlayerControlMove.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class scriptComponent : MonoBehaviour
 {
+
+    public float forwardSpeed = 0.05f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,6 @@ public class scriptComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.transform.Translate(new Vector3(0, 0, 0.05f));
+        transform.transform.Translate(new Vector3(0, 0, forwardSpeed));
     }
 }
